3f75f60ba1b26192363b3753015e9cfda3ac27f3
averybogusz <56933616+averybogusz@users.noreply.github.com>
2019-10-25 20:44:19 -0400

    Fixed mistake, added naive indicator
    Had number of columns of P determined not by K, but by the number of columns of X, which is obviously wrong. Also added "naive" indicator to (optionally) set covariance to 0.

--- NaiveBayesClassifier.R
+++ NaiveBayesClassifier.R
@@ -9,21 +9,21 @@ NaiveBayesClassifier <- R6Class('NaiveBayesClassifier',
     priors = hash(),
     K = NULL,
     P = NULL,
-    fit = function(X, y){
+    fit = function(X, y, naive = 0){
       self$K <- unique(y)
 
     
       for (k in self$K) {
         X_k <-matrix(X[y==k], ncol = ncol(X))
         self$means[[toString(k)]] <- colMeans(X_k)
-        self$sigmas[[toString(k)]] <- cov(X_k)
+        self$sigmas[[toString(k)]] <- (1 - naive) * cov(X_k) + naive * diag(diag(cov(X_k)))
         self$priors[[toString(k)]] <- length(X_k)/length(X)
     }
     
     },
     ## Predict definition 
     predict = function(X){
-      self$P <- matrix(0, ncol = ncol(X), nrow = nrow(X))
+      self$P <- matrix(0, ncol = length(self$K), nrow = nrow(X))
     
        for (k in self$K) {
           self$P[,k+1] <- dmvnorm(X, self$means[[toString(k)]], self$sigmas[[toString(k)]], log = TRUE) + log(self$priors[[toString(k)]])
@@ -33,5 +33,4 @@ NaiveBayesClassifier <- R6Class('NaiveBayesClassifier',
     }
   )
 )
-
-
+ 
